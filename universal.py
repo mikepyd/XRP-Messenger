6bdacfea04aa74030c663f5e964606652384c3b0
Michiel Odendaal <mikepydev@gmail.com>
2019-09-14 20:07:39 +0200

    Delete universal.py

--- universal.py
+++
@@ -1,9 +0,0 @@
-'''
-Universal is the initial value for the data in soupmaker. 
-The values are changed by soupmaker.py and it gets forwarded to filewriter.py.
-
-Author: Michiel Odendaal
-Last modification date: 08 Aug 2019
-'''
-#Initial value for history.
-status = 0
